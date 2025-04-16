40bb9aeb51320e85e4cc573372cf9a291918f652
Bayel-Tashpulatov <bayeltashpulatov1@gmail.com>
2025-04-16 08:38:12 +0600

    update project

--- script.js
+++ script.js
@@ -1,3 +1,17 @@
 document.getElementById("myButton").addEventListener("click", function() {
-    alert("Вы нажали на кнопку!");
+    // Показываем модальное окно
+    document.getElementById("myModal").style.display = "block";
+});
+
+// Закрываем модальное окно при нажатии на "x"
+document.querySelector(".close").addEventListener("click", function() {
+    document.getElementById("myModal").style.display = "none";
+});
+
+// Закрываем модальное окно при клике вне его
+window.addEventListener("click", function(event) {
+    const modal = document.getElementById("myModal");
+    if (event.target === modal) {
+        modal.style.display = "none";
+    }
 });
